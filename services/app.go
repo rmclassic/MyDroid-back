ada57a8b259b855b7242ba60fa7e57d4d981f8ec
رامین خونساری <ramin.khoonsari@sjicompany.com>
2021-03-06 12:26:52 +0330

    Best subtree as well

--- services/app.go
+++ services/app.go
@@ -70,15 +70,35 @@ func GetBest(c *gin.Context) {
 
   page, _ := strconv.Atoi(pagestr)
   per_page, _ := strconv.Atoi(per_pagestr)
+
+
+
+  apps := GetBestOfCategory(category, page, per_page)
+  c.JSON(200, gin.H{
+    "result": "success",
+    "data": apps,
+  })
+}
+
+func GetBestOfCategoryAndSubs(category string, page int, page_str int) []models.App {
+  apps := GetBestOfCategory(category, page, page_str)
+  subCats := GetSubCategories(category)
+
+  for _, subCat := range(subCats) {
+    subApps := GetBestOfCategoryAndSubs(subCat, page, page_str)
+    for _, sa := range(subApps) {
+      apps = append(apps, sa)
+    }
+  }
+
+  return apps
+}
+
+
+func GetBestOfCategory(category string, page int, per_page int) []models.App {
   query := fmt.Sprintf("SELECT *, COUNT(app_id) AS cnt FROM ((app JOIN download ON app.id=download.app_id) JOIN (select name as category, app_id as capp_id from app_category JOIN category ON category_id=id) cat ON cat.capp_id=app_id) WHERE category='%s' GROUP BY app_id ORDER BY cnt DESC LIMIT %d, %d", category, page, per_page)
   println(query)
-  rows, err := db.Query(query)
-  if err != nil {
-    c.JSON(200, gin.H{
-      "result": "fail",
-      "message": err,
-    })
-  }
+  rows, _ := db.Query(query)
 
   apps := make([]models.App, 0)
 
@@ -88,23 +108,14 @@ func GetBest(c *gin.Context) {
       var tempapp models.App
       var wow string
       rows.Scan(&tempapp.ID, &tempapp.Name, &tempapp.Description, &pid, &wow, &wow, &wow, &wow, &wow, &tempapp.Category, nil, nil)
-      publisher, err := GetAccountByID(pid)
-      if err != nil {
-        c.JSON(200, gin.H{
-          "result": "fail",
-          "message": err,
-        })
-      }
+      publisher, _ := GetAccountByID(pid)
 
       tempapp.ImageUrl =  fmt.Sprintf("/assets/thumbs/%d.jpg", tempapp.ID)
       tempapp.Publisher = publisher.Name
       apps = append(apps, tempapp)
     }
 
-    c.JSON(200, gin.H{
-      "result": "success",
-      "data": apps,
-    })
+    return apps
 }
 
 func GetLatest(c *gin.Context) {
